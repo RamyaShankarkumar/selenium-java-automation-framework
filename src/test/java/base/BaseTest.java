70ebfd45f18e21838db037936f458ad5a2e28285
RamyaShankarkumar <40883090+RamyaShankarkumar@users.noreply.github.com>
2025-12-26 12:38:20 -0500

    Create BaseTest.java

---
+++ src/test/java/base/BaseTest.java
@@ -0,0 +1,21 @@
+package base;
+
+import driver.DriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class BaseTest {
+
+    protected WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        driver = DriverFactory.createDriver();
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+}
