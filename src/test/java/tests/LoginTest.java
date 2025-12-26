ccb09ab72686d5a7dea8a2607117310025148a38
RamyaShankarkumar <40883090+RamyaShankarkumar@users.noreply.github.com>
2025-12-26 12:41:11 -0500

    Create LoginTest.java

---
+++ src/test/java/tests/LoginTest.java
@@ -0,0 +1,19 @@
+package tests;
+
+import base.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.LoginPage;
+
+public class LoginTest extends BaseTest {
+
+    @Test
+    public void validLoginTest() {
+        LoginPage login = new LoginPage(driver);
+
+        login.load();
+        login.login("admin", "password");
+
+        Assert.assertTrue(driver.getCurrentUrl().contains("dashboard"));
+    }
+}
