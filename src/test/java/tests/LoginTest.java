6b70be1d3e44a5cd8183bafe4ae70889a2f3be85
RamyaShankarkumar <40883090+RamyaShankarkumar@users.noreply.github.com>
2025-12-26 20:36:50 -0500

    Update LoginTest.java

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,9 +1,12 @@
 package tests;
 
-import base.BaseTest;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import java.time.Duration;
+
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import pages.LoginPage;
 
 public class LoginTest extends BaseTest {
 
