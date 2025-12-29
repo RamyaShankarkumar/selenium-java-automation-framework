96057b5767442866db9cacb8202d4e5aefd63225
RamyaShankarkumar <40883090+RamyaShankarkumar@users.noreply.github.com>
2025-12-29 14:10:38 -0500

    Update LoginPage.java

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -1,27 +1,16 @@
 package pages;
 
-import config.ConfigReader;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import java.time.Duration;
 
 public class LoginPage {
-
     WebDriver driver;
-    WebDriverWait wait;
-
     By username = By.id("user-name");
     By password = By.id("password");
     By loginBtn = By.id("login-button");
 
     public LoginPage(WebDriver driver) {
         this.driver = driver;
-        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-    }
-
-    public void load() {
-        driver.get(ConfigReader.get("base.url") + "/login");
     }
 
     public void login(String user, String pwd) {
