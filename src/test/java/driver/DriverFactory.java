09cd5e00064ab2ce962bcac9205eb17a6367a9d1
RamyaShankarkumar <40883090+RamyaShankarkumar@users.noreply.github.com>
2025-12-29 14:19:52 -0500

    Update DriverFactory.java

--- src/test/java/driver/DriverFactory.java
+++ src/test/java/driver/DriverFactory.java
@@ -1,28 +1,20 @@
 package driver;
 
-import config.ConfigReader;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 public class DriverFactory {
 
-    public static WebDriver createDriver() {
+    public static WebDriver getDriver() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--window-size=1920,1080");
 
-        String browser = ConfigReader.get("browser");
-
-        if (browser.equalsIgnoreCase("chrome")) {
-            WebDriverManager.chromedriver().setup();
-
-            ChromeOptions options = new ChromeOptions();
-            if (ConfigReader.get("headless").equals("true")) {
-                options.addArguments("--headless");
-            }
-
-            return new ChromeDriver(options);
-        }
-
-        throw new RuntimeException("Unsupported browser");
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("https://www.saucedemo.com/");
+        return driver;
     }
 }
