356f7ea5a96bfdb030e0d240beb511af0e00d36e
RamyaShankarkumar <40883090+RamyaShankarkumar@users.noreply.github.com>
2025-12-29 14:10:16 -0500

    Update BaseTest.java

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,21 +1,29 @@
 package base;
 
-import driver.DriverFactory;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 public class BaseTest {
-
     protected WebDriver driver;
 
     @BeforeMethod
     public void setUp() {
-        driver = DriverFactory.createDriver();
+        ChromeOptions options = new ChromeOptions();
+        // CI-safe options
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--window-size=1920,1080");
+
+        driver = new ChromeDriver(options);
+        driver.get("https://www.saucedemo.com/");
     }
 
     @AfterMethod
     public void tearDown() {
-        driver.quit();
+        if (driver != null) driver.quit();
     }
 }
