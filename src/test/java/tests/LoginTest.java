b1d4551170b74b2240187adf613bd8152c403357
RamyaShankarkumar <40883090+RamyaShankarkumar@users.noreply.github.com>
2025-12-29 14:11:04 -0500

    Update LoginTest.java

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,30 +1,22 @@
 package tests;
 
 import base.BaseTest;
-import org.openqa.selenium.By;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import pages.LoginPage;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import java.time.Duration;
-
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import pages.LoginPage;
-
+import java.time.Duration;
 
 public class LoginTest extends BaseTest {
-
     @Test
     public void validLoginTest() {
         LoginPage login = new LoginPage(driver);
-
-        login.load();
         login.login("standard_user", "secret_sauce");
-        
-        /*WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         wait.until(ExpectedConditions.urlContains("inventory"));
-        Assert.assertTrue(driver.findElement(By.className("title")).isDisplayed());*/
 
         Assert.assertTrue(driver.getCurrentUrl().contains("inventory"));
-
     }
 }
