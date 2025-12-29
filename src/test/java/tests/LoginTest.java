a3866dbe733c06dff77da11e9b54432638788773
RamyaShankarkumar <40883090+RamyaShankarkumar@users.noreply.github.com>
2025-12-29 14:20:59 -0500

    Update LoginTest.java

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -2,13 +2,15 @@ package tests;
 
 import base.BaseTest;
 import pages.LoginPage;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
 import java.time.Duration;
 
 public class LoginTest extends BaseTest {
+
     @Test
     public void validLoginTest() {
         LoginPage login = new LoginPage(driver);
@@ -17,6 +19,7 @@ public class LoginTest extends BaseTest {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         wait.until(ExpectedConditions.urlContains("inventory"));
 
-        Assert.assertTrue(driver.getCurrentUrl().contains("inventory"));
+        Assert.assertTrue(driver.getCurrentUrl().contains("inventory"),
+                "Login failed: inventory page not loaded");
     }
 }
